53aa43e39f997e6afa6591a86299cab74cde2722
Jeff Buck <jsb@air.home>
2010-05-28 02:26:06 -0400

    Cleaned up code structure

--- redisspy.c
+++ redisspy.c
@@ -60,9 +60,26 @@ typedef struct
 
 typedef struct
 {
-	REDISDATA*	data;
+	REDISDATA*		data;
+	unsigned int	keyCount;
 
+	char			pattern[REDIS_MAX_PATTERN_LEN];
+
+	int				infoConnectedClients;
+	char			infoUsedMemoryHuman[32];
+
+	int				sortBy;
+	int				sortReverse;
+
+	int				refreshInterval;
+
+	char			host[REDIS_MAX_HOST_LEN];
+	unsigned int	port;
+
+	int				deadServer;
 } REDIS;
+
+REDIS* redis;
 
 // Curses data
 #define MAXROWS	1000
@@ -73,37 +90,22 @@ static unsigned int	screenCols;
 static WINDOW*		screen;
 static unsigned int displayRows;
 
-static REDISDATA*	redisData;
-static unsigned int	redisRows;
-static int			deadServer;
 
 static unsigned int	currentRow = 0;
 //static unsigned int	currentCol = 0;
 static unsigned int	startIndex = 0;
 
-static char			host[REDIS_MAX_HOST_LEN];
-static unsigned int	port;
 
 #define sortByKey		1
 #define sortByType		2
 #define sortByLength	3
 #define sortByValue		4
 
-static char			pattern[REDIS_MAX_PATTERN_LEN];
-
-static int			infoConnectedClients;
-static char			infoUsedMemoryHuman[32];
-
-static int			sortBy;
-static int			sortReverse;
-
-static int			refreshInterval;
-
 
 // Sort functions
-#define SWAPIFREVERSESORT(x, y) \
+#define SWAPIFREVERSESORT(thunk, x, y) \
 	const void* tmp; \
-	if (sortReverse) \
+	if (((REDIS*)thunk)->sortReverse) \
 	{ \
 		tmp = y; \
 		y = x; \
@@ -114,51 +116,52 @@ static int			refreshInterval;
 #define safestrcat(d, s) \
 	strncat(d, s, sizeof(d) - strlen(s) - 1);
 
-int compareKeys(const void* a, const void* b)
+
+int compareKeys(void* thunk, const void* a, const void* b)
 {
-	SWAPIFREVERSESORT(a, b);
+	SWAPIFREVERSESORT(thunk, a, b);
 
 	return strcmp(((REDISDATA*)a)->key, ((REDISDATA*)b)->key);
 }
 
-int compareTypes(const void* a, const void* b)
+int compareTypes(void* thunk, const void* a, const void* b)
 {
-	SWAPIFREVERSESORT(a, b);
+	SWAPIFREVERSESORT(thunk, a, b);
 
 	int r = strcmp(((REDISDATA*)a)->type, ((REDISDATA*)b)->type);
 
 	if (r == 0)
-		return compareKeys(a, b);
+		return compareKeys(thunk, a, b);
 	
 	return r;
 }
 
-int compareLengths(const void* a, const void* b)
+int compareLengths(void* thunk, const void* a, const void* b)
 {
-	SWAPIFREVERSESORT(a, b);
+	SWAPIFREVERSESORT(thunk, a, b);
 
 	int r = ((REDISDATA*)b)->length - ((REDISDATA*)a)->length;
 
 	if (r == 0)
-		return compareKeys(a, b);
+		return compareKeys(thunk, a, b);
 	
 	return r;
 }
 
-int compareValues(const void* a, const void* b)
+int compareValues(void* thunk, const void* a, const void* b)
 {
-	SWAPIFREVERSESORT(a, b);
+	SWAPIFREVERSESORT(thunk, a, b);
 
 	int r = strcmp(((REDISDATA*)a)->value, ((REDISDATA*)b)->value);
 
 	if (r == 0)
-		return compareKeys(a, b);
+		return compareKeys(thunk, a, b);
 	
 	return r;
 }
 
 // Screen drawing
-void redisplay()
+void redisplay(REDIS* redis)
 {
 	unsigned int i = 0;
 	unsigned int redisIndex = startIndex;
@@ -184,35 +187,35 @@ void redisplay()
 	//attroff(A_UNDERLINE);
 
 	i = 0;
-	while ((i < displayRows) && (redisIndex < redisRows))
+	while ((i < displayRows) && (redisIndex < redis->keyCount))
 	{
 		char line[MAXCOLS];
 		sprintf(line, "%-20s  %-6s  %6d  ",
-				redisData[redisIndex].key,
-				redisData[redisIndex].type,
-				redisData[redisIndex].length);
-		strncat(line, redisData[redisIndex].value, screenCols - 38);
+				redis->data[redisIndex].key,
+				redis->data[redisIndex].type,
+				redis->data[redisIndex].length);
+		strncat(line, redis->data[redisIndex].value, screenCols - 38);
 
 		mvaddstr(i+1, 0, line); // skip the header row
 		++i;
 		++redisIndex;
 	}
 
-	if (deadServer)
+	if (redis->deadServer)
 	{
 		sprintf(status, "[host=%s:%d] No Connection.", 
-			host, 
-			port); 
+			redis->host, 
+			redis->port); 
 	}
 	else
 	{
 		sprintf(status, "[host=%s:%d] [keys=%d] [%d%%] [clients=%d] [mem=%s]", 
-			host, 
-			port, 
-			redisRows, 
-			redisRows ? redisIndex*100/redisRows : 0,
-			infoConnectedClients, 
-			infoUsedMemoryHuman);
+			redis->host, 
+			redis->port, 
+			redis->keyCount, 
+			redis->keyCount ? redisIndex*100/redis->keyCount : 0,
+			redis->infoConnectedClients, 
+			redis->infoUsedMemoryHuman);
 	}
 
 	for (i = strlen(status) - 1; i < screenCols - 1; i++)
@@ -245,15 +248,15 @@ void quitCurses()
 }
 
 
-void highlightCurrentRow()
+void highlightCurrentRow(REDIS* redis)
 {
 	attron(A_BOLD);
-	mvaddstr(currentRow, 0, redisData[currentRow].key);
+	mvaddstr(currentRow, 0, redis->data[currentRow].key);
 	attroff(A_BOLD);
 	refresh();
 }
 
-void pageup()
+void pageup(REDIS* redis)
 {
 	if (((int)startIndex - (int)displayRows) < 0)
 	{
@@ -265,12 +268,12 @@ void pageup()
 		startIndex -= displayRows;
 	}
 
-	redisplay();
+	redisplay(redis);
 }
 
-void pagedown()
+void pagedown(REDIS* redis)
 {
-	if ((startIndex + displayRows) > redisRows)
+	if ((startIndex + displayRows) > redis->keyCount)
 	{
 		beep();
 	}
@@ -279,7 +282,7 @@ void pagedown()
 		startIndex += displayRows;
 	}
 
-	redisplay();
+	redisplay(redis);
 }
 
 
@@ -296,19 +299,19 @@ void help()
 }
 
 // Redis functions
-int redisRefresh(char* pattern)
+int redisRefresh(REDIS* redis)
 {
 	int fd = -1;
 	redisReply* r = NULL;
 
-	r = redisConnect(&fd, host, port);
+	r = redisConnect(&fd, redis->host, redis->port);
 	if (r != NULL)
 	{
-		deadServer = 1;
+		redis->deadServer = 1;
 		return -1;
 	}
 
-	deadServer = 0;
+	redis->deadServer = 0;
 
 	// Put a busy signal on the status line
 	attron(A_STANDOUT);
@@ -316,8 +319,8 @@ int redisRefresh(char* pattern)
 	attroff(A_STANDOUT);
 	refresh();
 
-	infoConnectedClients = 0;
-	strcpy(infoUsedMemoryHuman, "");
+	redis->infoConnectedClients = 0;
+	strcpy(redis->infoUsedMemoryHuman, "");
 
 	r = redisCommand(fd, "INFO");
 	if (r->type == REDIS_REPLY_STRING)
@@ -332,7 +335,7 @@ int redisRefresh(char* pattern)
 			t = strtok(NULL, ":\r\n");
 
 			if (t)
-				infoConnectedClients = atoi(t);
+				redis->infoConnectedClients = atoi(t);
 		}
 
 		if (m)
@@ -341,37 +344,43 @@ int redisRefresh(char* pattern)
 			t = strtok(NULL, ":\r\n");
 
 			if (t)
-				strcpy(infoUsedMemoryHuman, t);
+				strcpy(redis->infoUsedMemoryHuman, t);
 		}
 	}
 
 
-	r = redisCommand(fd, "KEYS %s", pattern);
+	if (redis->pattern[0] == '\0')
+	{
+		strcpy(redis->pattern, "*");
+	}
+
+
+	r = redisCommand(fd, "KEYS %s", redis->pattern);
 	if (r->type == REDIS_REPLY_ARRAY)
 	{
 		// If our current buffer is big enough, don't bother
 		// freeing and mallocing a new one every time.
-		if (redisData && (r->elements > redisRows))
+		if (redis->data && (r->elements > redis->keyCount))
 		{
-			free(redisData);
-			redisData = NULL;
+			free(redis->data);
+			redis->data = NULL;
 		}
 
-		if (redisData == NULL)
-			redisData = malloc(r->elements * sizeof(REDISDATA));
+		if (redis->data == NULL)
+			redis->data = malloc(r->elements * sizeof(REDISDATA));
 
-		redisRows = r->elements;
+		redis->keyCount = r->elements;
 
 		for (unsigned i = 0; i < r->elements; i++)
 		{
-			strcpy(redisData[i].key, r->element[i]->reply);
-			redisData[i].length = 0;
-			redisData[i].value[0] = '\0';
+			strcpy(redis->data[i].key, r->element[i]->reply);
+			redis->data[i].length = 0;
+			redis->data[i].value[0] = '\0';
 
 			redisReply* t = redisCommand(fd, "TYPE %s", 
 			                             r->element[i]->reply);
 
-			strcpy(redisData[i].type, t->reply);
+			strcpy(redis->data[i].type, t->reply);
 
 			redisReply* v = NULL;
 
@@ -379,69 +388,69 @@ int redisRefresh(char* pattern)
 			{
 				v = redisCommand(fd, "GET %s", r->element[i]->reply);
 
-				strcpy(redisData[i].value, v->reply);
-				redisData[i].length = strlen(v->reply);
+				strcpy(redis->data[i].value, v->reply);
+				redis->data[i].length = strlen(v->reply);
 			}
 			else if (strcmp(t->reply, "list") == 0)
 			{
 				v = redisCommand(fd, "LRANGE %s 0 -1", r->element[i]->reply);
 
-				redisData[i].length = v->elements;
+				redis->data[i].length = v->elements;
 
 				for (unsigned j = 0; j < v->elements; j++)
 				{
 					if (j > 0)
-						safestrcat(redisData[i].value, " ");
+						safestrcat(redis->data[i].value, " ");
 
-					safestrcat(redisData[i].value, v->element[j]->reply);
+					safestrcat(redis->data[i].value, v->element[j]->reply);
 				}
 			}
 			else if (strcmp(t->reply, "hash") == 0)
 			{
 				v = redisCommand(fd, "HGETALL %s", r->element[i]->reply);
 
-				redisData[i].length = v->elements >> 1;
+				redis->data[i].length = v->elements >> 1;
 
 				for (unsigned j = 0; j < v->elements; j+=2)
 				{
 					if (j > 0)
-						safestrcat(redisData[i].value, " ");
+						safestrcat(redis->data[i].value, " ");
 
-					safestrcat(redisData[i].value, v->element[j]->reply);
-					safestrcat(redisData[i].value, "->");
+					safestrcat(redis->data[i].value, v->element[j]->reply);
+					safestrcat(redis->data[i].value, "->");
 					/*
 					if (strcmp(v->element[j+1]->reply, "") == 0)
 						safestrcat(value, "(nil)");
 					else
 						safestrcat(value, v->element[j+1]->reply);
 					*/
-					safestrcat(redisData[i].value, v->element[j+1]->reply);
+					safestrcat(redis->data[i].value, v->element[j+1]->reply);
 				}
 			}
 			else if (strcmp(t->reply, "set") == 0)
 			{
 				v = redisCommand(fd, "SMEMBERS %s", r->element[i]->reply);
-				redisData[i].length = v->elements;
+				redis->data[i].length = v->elements;
 
 				for (unsigned j = 0; j < v->elements; j++)
 				{
 					if (j > 0)
-						safestrcat(redisData[i].value, " ");
+						safestrcat(redis->data[i].value, " ");
 
-					safestrcat(redisData[i].value, v->element[j]->reply);
+					safestrcat(redis->data[i].value, v->element[j]->reply);
 				}
 			}
 			else if (strcmp(t->reply, "zset") == 0)
 			{
 				v = redisCommand(fd, "ZRANGE %s 0 -1", r->element[i]->reply);
-				redisData[i].length = v->elements;
+				redis->data[i].length = v->elements;
 
 				for (unsigned j = 0; j < v->elements; j++)
 				{
 					if (j > 0)
-						safestrcat(redisData[i].value, " ");
+						safestrcat(redis->data[i].value, " ");
 
-					safestrcat(redisData[i].value, v->element[j]->reply);
+					safestrcat(redis->data[i].value, v->element[j]->reply);
 				}
 			}
 
@@ -460,36 +469,36 @@ int redisRefresh(char* pattern)
 	return 0;
 }
 
-void redisSort(int newSortBy)
+void redisSort(REDIS* redis, int newSortBy)
 {
 	// 0 means repeat what we did last time
 	if (newSortBy > 0) 
 	{
-		if (sortBy == newSortBy)
-			sortReverse = ~sortReverse;
+		if (redis->sortBy == newSortBy)
+			redis->sortReverse = ~redis->sortReverse;
 		else
 		{
-			sortReverse = 0;
-			sortBy = newSortBy;
+			redis->sortReverse = 0;
+			redis->sortBy = newSortBy;
 		}
 	}
 
-	switch (sortBy)
+	switch (redis->sortBy)
 	{
 		case sortByKey:
-			qsort(redisData, redisRows, sizeof(REDISDATA), compareKeys);
+			qsort_r(redis->data, redis->keyCount, sizeof(REDISDATA), redis, compareKeys);
 			break;
 
 		case sortByType:
-			qsort(redisData, redisRows, sizeof(REDISDATA), compareTypes);
+			qsort_r(redis->data, redis->keyCount, sizeof(REDISDATA), redis, compareTypes);
 			break;
 
 		case sortByLength:
-			qsort(redisData, redisRows, sizeof(REDISDATA), compareLengths);
+			qsort_r(redis->data, redis->keyCount, sizeof(REDISDATA), redis, compareLengths);
 			break;
 
 		case sortByValue:
-			qsort(redisData, redisRows, sizeof(REDISDATA), compareValues);
+			qsort_r(redis->data, redis->keyCount, sizeof(REDISDATA), redis, compareValues);
 			break;
 	}
 }
@@ -498,10 +507,10 @@ void redisSort(int newSortBy)
 
 void timerExpired(int UNUSED(i))
 {
-	redisRefresh(pattern);
-	redisSort(0);
+	redisRefresh(redis);
+	redisSort(redis, 0);
 
-	redisplay();
+	redisplay(redis);
 
 /*
 	signal(SIGALRM, timerExpired);
@@ -512,7 +521,7 @@ void timerExpired(int UNUSED(i))
 */
 }
 
-void resetTimer(int interval)
+void resetTimer(REDIS* redis, int interval)
 {
 	if (interval == 0)
 	{
@@ -524,12 +533,12 @@ void resetTimer(int interval)
 		signal(SIGALRM, timerExpired);
 	}
 
-	refreshInterval = interval;
+	redis->refreshInterval = interval;
 
 	struct itimerval timer;
-	timer.it_interval.tv_sec = refreshInterval;
+	timer.it_interval.tv_sec = redis->refreshInterval;
 	timer.it_interval.tv_usec = 0;
-	timer.it_value.tv_sec = refreshInterval;
+	timer.it_value.tv_sec = redis->refreshInterval;
 	timer.it_value.tv_usec = 0;
 	setitimer(ITIMER_REAL, &timer, NULL);
 }
@@ -623,7 +632,7 @@ int getCommand(char* prompt, char* str, int max)
 	for (unsigned int i = 0; i < screenCols - 1; i++)
 		mvaddstr(row, i, " ");
 
-	if (refreshInterval)
+	if (redis->refreshInterval)
 		signal(SIGALRM, timerExpired);
 
 	return 0;
@@ -632,12 +641,14 @@ int getCommand(char* prompt, char* str, int max)
 
 int main(int argc, char* argv[])
 {
-	strcpy(host, defaultHost);
-	port = defaultPort;
-	strcpy(pattern, defaultPattern);
+	redis = malloc(sizeof(REDIS));
+
+	strcpy(redis->host, defaultHost);
+	redis->port = defaultPort;
+	strcpy(redis->pattern, defaultPattern);
 
 	char refreshIntervalBuffer[80];
-	refreshInterval = 0;
+	redis->refreshInterval = 0;
 
 	int c; 
 	while ((c = getopt(argc, argv, "h:p:a:k:?")) != -1)
@@ -645,19 +656,19 @@ int main(int argc, char* argv[])
 		switch (c)
 		{
 			case 'h':
-				strcpy(host, optarg);
+				strcpy(redis->host, optarg);
 				break;
 
 			case 'p':
-				port = atoi(optarg);
+				redis->port = atoi(optarg);
 				break;
 
 			case 'k':
-				strcpy(pattern, optarg);
+				strcpy(redis->pattern, optarg);
 				break;
 
 			case 'a':
-				refreshInterval = atoi(optarg);
+				redis->refreshInterval = atoi(optarg);
 				break;
 
 			case '?':
@@ -672,23 +683,23 @@ int main(int argc, char* argv[])
 	argv += optind;
 
 
-	sortReverse = 0;
-	sortBy = sortByKey;
+	redis->sortReverse = 0;
+	redis->sortBy = sortByKey;
 
 
 	initCurses();
 
 	
-	if (refreshInterval)
+	if (redis->refreshInterval)
 	{
-		resetTimer(refreshInterval);
+		resetTimer(redis, redis->refreshInterval);
 	}
 	else
 	{
-		redisRefresh(pattern);
-		redisSort(sortByKey);
+		redisRefresh(redis);
+		redisSort(redis, sortByKey);
 
-		redisplay();
+		redisplay(redis);
 	}
 
 	int done = 0;
@@ -699,7 +710,7 @@ int main(int argc, char* argv[])
 		switch(d)
 		{
 			case KEY_RESIZE:
-				redisplay();
+				redisplay(redis);
 				break;
 
 			case 'q':
@@ -707,11 +718,11 @@ int main(int argc, char* argv[])
 				break;
 
 			case 'p':
-				if (getCommand("Pattern: ", pattern, sizeof(pattern)) == 0)
+				if (getCommand("Pattern: ", redis->pattern, sizeof(redis->pattern)) == 0)
 				{
-					redisRefresh(pattern);
-					redisSort(0);
-					redisplay();
+					redisRefresh(redis);
+					redisSort(redis, 0);
+					redisplay(redis);
 				}
 				break;
 
@@ -720,49 +731,49 @@ int main(int argc, char* argv[])
 						refreshIntervalBuffer, 
 						sizeof(refreshIntervalBuffer)) == 0)
 				{
-					refreshInterval = atoi(refreshIntervalBuffer);
-					resetTimer(refreshInterval);
+					int interval = atoi(refreshIntervalBuffer);
+					resetTimer(redis, interval);
 				}
 				break;
 
 			case 'd':
-				redisplay();
+				redisplay(redis);
 				break;
 
 			case 'r':
-				redisRefresh(pattern);
-				redisSort(0);
-				redisplay();
+				redisRefresh(redis);
+				redisSort(redis, 0);
+				redisplay(redis);
 				break;
 
 			case 'k':
-				redisSort(sortByKey);
-				redisplay();
+				redisSort(redis, sortByKey);
+				redisplay(redis);
 				break;
 
 			case 't':
-				redisSort(sortByType);
-				redisplay();
+				redisSort(redis, sortByType);
+				redisplay(redis);
 				break;
 
 			case 'l':
-				redisSort(sortByLength);
-				redisplay();
+				redisSort(redis, sortByLength);
+				redisplay(redis);
 				break;
 
 			case 'v':
-				redisSort(sortByValue);
-				redisplay();
+				redisSort(redis, sortByValue);
+				redisplay(redis);
 				break;
 
 
 			case 'f':
 			case ' ':
-				pagedown();
+				pagedown(redis);
 				break;
 
 			case 'b':
-				pageup();
+				pageup(redis);
 				break;
 
 			case '?':
